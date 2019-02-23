5586f45e3b30a1e41d4d1c3760f88c774ada859f
Akshay Badak <ab44281@gmail.com>
2019-02-23 10:22:31 +0530

    initial commit

---
+++ api/angular_db.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 23, 2019 at 05:46 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `angular_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cars`
+--
+
+CREATE TABLE `cars` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `model` varchar(255) NOT NULL DEFAULT '',
+  `price` int(10) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `cars`
+--
+
+INSERT INTO `cars` (`id`, `model`, `price`) VALUES
+(1, 'abc', 12000),
+(3, 'xyz', 1234),
+(4, 'motor', 200000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cars`
+--
+ALTER TABLE `cars`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,85 @@
+import { Component } from '@angular/core';
+
+import { Car } from './car';
+import { CarService } from './car.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'cars';
+
+  cars: Car[];
+  error = '';
+  success = '';
+
+  car = new Car('', 0);
+
+  constructor(private carService: CarService) {}
+
+  ngOnInit() {
+    this.getCars();
+  }
+
+  getCars(): void {
+    this.carService.getAll().subscribe(
+      (res: Car[]) => {
+        this.cars = res;
+      },
+      (err) => {
+        this.error = err;
+      }
+    );
+  }
+
+  addCar(f) {
+    this.error = '';
+    this.success = '';
+
+    this.carService.store(this.car)
+      .subscribe(
+        (res: Car[]) => {
+          // Update the list of cars
+          this.cars = res;
+
+          // Inform the user
+          this.success = 'Created successfully';
+
+          // Reset the form
+          f.reset();
+        },
+        (err) => this.error = err
+      );
+}
+
+updateCar(name, price, id) {
+  this.success = '';
+  this.error = '';
+
+  this.carService.update({ model: name.value, price: price.value, id: +id })
+    .subscribe(
+      (res) => {
+        this.cars    = res;
+        this.success = 'Updated successfully';
+      },
+      (err) => this.error = err
+    );
+}
+
+deleteCar(id) {
+  this.success = '';
+  this.error   = '';
+
+  this.carService.delete(+id)
+    .subscribe(
+      (res: Car[]) => {
+        this.cars = res;
+        this.success = 'Deleted successfully';
+      },
+      (err) => this.error = err
+    );
+}
+
+}
